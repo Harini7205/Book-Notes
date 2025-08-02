64f71bf4d3ab908bebf6ad3cfd44193af07cdfd0
Harini <87068879+Harini7205@users.noreply.github.com>
2025-08-02 19:11:37 +0530

    Delete dbConfig.js

--- dbConfig.js
+++
@@ -1,9 +0,0 @@
-import pkg from 'pg';
-const {Pool}=pkg;
-const itemsPool = new Pool({
-    connectionString: process.env.DBConfigLink,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-export default itemsPool;
